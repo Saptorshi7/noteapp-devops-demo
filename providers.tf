84a4c5fcb5ea2014c26bb10d65acd6df2390859f
Saptorshi7 <saptorshi98@gmail.com>
2025-09-02 22:48:13 +0530

    clean-up

--- providers.tf
+++ providers.tf
@@ -5,12 +5,4 @@ terraform {
       version = "=4.41.0"
     }
   }
-
-  # backend "azurerm" {
-  #   resource_group_name   = "terraform-state"
-  #   storage_account_name  = "tfstate0911"
-  #   container_name        = "tfstate"
-  #   key                   = "noteapp.terraform.tfstate"
-  # }
-
 }
