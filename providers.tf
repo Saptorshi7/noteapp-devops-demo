21316ff5feebb03f34f831586f2f2d88ed2879ff
Saptorshi7 <saptorshi98@gmail.com>
2025-09-02 21:32:27 +0530

    moved backend

--- providers.tf
+++ providers.tf
@@ -5,4 +5,12 @@ terraform {
       version = "=4.41.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name   = "terraform-state"
+    storage_account_name  = "tfstate0911"
+    container_name        = "tfstate"
+    key                   = "noteapp.terraform.tfstate"
+  }
+
 }
