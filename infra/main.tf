03b0aea2cedb2ddb4ced48bd536096a6fff73c40
Saptorshi7 <saptorshi98@gmail.com>
2025-09-04 14:27:02 +0530

    modularize tf

--- infra/main.tf
+++ infra/main.tf
@@ -7,15 +7,15 @@ provider "azurerm" {
 module "rg" {
   source              = "./modules/rg"
 
-  name     = "react-app-rg"
-  location = "South India"
+  name     = var.rg_name
+  location = var.location
 }
 
 # Azure Kubernetes Service (AKS)
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "react-app-aks"
-  location            = module.rg.location
-  resource_group_name = module.rg.name
+  location            = var.location
+  resource_group_name = var.rg_name
   dns_prefix          = "react-app"
 
   default_node_pool {
@@ -32,7 +32,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 # Azure Container Registry (ACR)
 resource "azurerm_container_registry" "acr" {
   name                = "reactappacr47"
-  resource_group_name = module.rg.name
-  location            = module.rg.location
+  resource_group_name = var.rg_name
+  location            = var.location
   sku                 = "Basic"
 }
