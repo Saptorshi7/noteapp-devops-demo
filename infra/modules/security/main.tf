131ed60316d59a662238abd23dba3b60c47e3e2c
Saptorshi7 <saptorshi98@gmail.com>
2025-09-17 01:21:42 +0530

    add key size

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -37,6 +37,7 @@ resource "azurerm_key_vault_key" "example" {
   key_type     = var.key_type
   key_opts = var.key_opts
   expiration_date = var.expiration_date
+  key_size = "2048"
 }
 
 resource "azurerm_disk_encryption_set" "example" {
