c36e9986e90abf330f4528016f8ff51fc393a0a9
Saptorshi7 <saptorshi98@gmail.com>
2025-09-16 23:22:10 +0530

    add azurerm_key_vault_access_policy

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -14,13 +14,21 @@ resource "azurerm_key_vault" "example" {
                    bypass = var.network_acls_bypass
                   }
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+  # access_policy {
+  #   tenant_id = data.azurerm_client_config.current.tenant_id
+  #   object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions = var.key_permissions
-  }
+  #   key_permissions = var.key_permissions
+  # }
 
+}
+
+resource "azurerm_key_vault_access_policy" "example" {
+  key_vault_id = azurerm_key_vault.example.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = var.key_permissions
 }
 
 resource "azurerm_key_vault_key" "example" {
