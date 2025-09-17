c743f89e95b2974c197645be9caa5c8971bc825f
Saptorshi7 <saptorshi98@gmail.com>
2025-09-17 17:54:56 +0530

    create key policy for des

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_key_vault_access_policy" "des_policy" {
   key_vault_id = azurerm_key_vault.example.id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = azurerm_disk_encryption_set.des.identity[0].principal_id
+  object_id = azurerm_disk_encryption_set.example.identity[0].principal_id
 
   key_permissions = [
     "Get",
